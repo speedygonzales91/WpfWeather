bae958b0d0259b757fbb92bc1c1d7c94c30bf1e3
Zsolt Molnár <mozb17@gmail.com>
2019-11-06 11:22:30 +0100

    Added View, VM, Model classes.

--- View/AccuWeatherWindow.xaml.cs
+++ View/AccuWeatherWindow.xaml.cs
@@ -10,17 +10,16 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace WpfWeather
+namespace WpfWeather.View
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for AccuWeatherWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class AccuWeatherWindow : Window
     {
-        public MainWindow()
+        public AccuWeatherWindow()
         {
             InitializeComponent();
         }
---
+++ ViewModel/AccuWeatherApi.cs
@@ -0,0 +1,37 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using WpfWeather.Model;
+
+namespace WpfWeather.ViewModel
+{
+    public class AccuWeatherApi
+    {
+        public const string BASE_URL = "http://dataservice.accuweather.com/forecasts/v1/daily/5day/{0}";
+
+        public async Task<AccuWeather> GetWeatherAsync(int locationKey)
+        {
+            AccuWeather weather = new AccuWeather();
+            string url = string.Format(BASE_URL, locationKey);
+
+            UriBuilder builder = new UriBuilder(url);
+            builder.Query = $"apikey={ConfigurationManager.AppSettings["API_KEY"]}&metric=true";
+
+            using (HttpClient client = new HttpClient())
+            {
+                var response = await client.GetAsync(builder.Uri);
+                var json = await response.Content.ReadAsStringAsync();
+
+                weather = JsonConvert.DeserializeObject<AccuWeather>(json);
+            }
+
+            return weather;
+
+        }
+    }
+}
